e855d79b0f040b2cc42a2dc6db681489ff8c4bc5
mpun1426 <mpun1426@gmail.com>
2022-10-20 22:10:59 +0900

    [create]rooms controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   get 'users/index'
   get 'users/account'
   get 'users/profile'
   get 'users/sign_up' => 'users#new'
   get 'airphp/index'
   resources :users
+  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

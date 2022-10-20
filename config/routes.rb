c138660a1c22f547d05464aafa0f37d9579b5475
mpun1426 <mpun1426@gmail.com>
2022-10-20 18:47:45 +0900

    [update]gem bcrypt install

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/account'
   get 'users/profile'
+  get 'users/sign_up' => 'users#new'
   get 'airphp/index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
